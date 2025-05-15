b2fb74e9545317d79fbe2b33bbcfa472473e375b
omar albarakeh <omaralbarakeh2@gmail.com>
2025-05-16 02:58:00 +0300

    ready to go hosted 3

--- server5.js
+++ server5.js
@@ -59,7 +59,7 @@ const allowedOrigins = [
   "http://localhost:3000",
   "https://ngo-backend-p0rc.onrender.com",
   "https://test.sospalestine.fr",
-  "https://ngo-backend-reciept-email.onrender.com",
+  "https://ngo-frontend-reciept-email.vercel.app",
 ];
 
 app.use(
